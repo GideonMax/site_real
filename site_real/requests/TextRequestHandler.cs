40503a2c249b156aa82ef23f03c94349411771f3
GideonMax <45212668+GideonMax@users.noreply.github.com>
2020-03-19 18:34:55 +0200

    added text request handler in preparation for implementation

---
+++ site_real/requests/TextRequestHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Web.Http;
+using Newtonsoft.Json;
+
+namespace site_real.requests
+{
+    /*public class TextRequestHandler : HttpMessageHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage message, CancellationToken token)
+        {
+            HttpResponseMessage response = new HttpResponseMessage();
+            Console.WriteLine(message.RequestUri.LocalPath);
+
+            var parsedUri = message.RequestUri.LocalPath.Split('/');
+        }
+    }*/
+}
