15b916d9c9e687ad31282260d3c22a67eee9dce7
GideonMax <45212668+GideonMax@users.noreply.github.com>
2020-03-22 14:01:57 +0200

    added custom element that gets texts from the database

---
+++ site_real/js/DBTextWC.js
@@ -0,0 +1,16 @@
+﻿class DBTextdiv extends HTMLElement{
+    static get observedAttributes(){
+        return ['TextName'];
+    }
+    constructor(){
+        super();
+        this.div=document.createElement('div');
+        this.appendChild(this.div);
+    }
+    connectedCallback(){
+        var name = this.getAttribute('TextName');
+        GetText(name).then(val=>{this.div.innerHTML=val});
+    }
+}
+
+window.customElements.define("db-div",DBTextdiv)
